245a7d39d44cdbe7c8a3f2e6c19ddcd0b92d78b5
mrneko-OG <thetruemrneko@gmail.com>
2026-06-30 21:22:52 +0200

    Add files via upload

---
+++ stardance/script.js
@@ -0,0 +1,94 @@
+//background
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol {
+    constructor(x, y, fontSize, canvasHeight){
+        this.characters = "ハッカー01ハッカーNekoハッカー♔♕♖ハッカー01ハッカー♗♘♙ハッカー♚♛♜ハッカー01ハッカー♝♞♟猫";
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = "";
+        this.canvasHeight = canvasHeight;
+    }
+    draw(context){
+        this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
+        context.fillStyle = "#ff2700";
+        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98){
+            this.y = 0;
+        }
+        else {
+            this.y += 1;
+        }
+    }   
+}
+
+class Effect {
+    constructor(canvasWidth, canvasHeight){
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.fontSize = 25;
+        this.columns = this.canvasWidth/this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+        console.log(this.symbols);
+    }
+    #initialize(){
+        for (let i = 0; i < this.columns; i++){
+            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+        }
+    }
+    resize(width, height){
+        this.canvasHeight = width;
+        this.canvasHeight = height;
+        this.columns = this.canvasWidth/this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+    }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 30;
+const nextFrame = 1000/fps;
+let timer = 0;
+
+function animate(timeStamp){
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    if (timer > nextFrame){
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.textAlign = "center";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    ctx.font = effect.fontSize + "px monospace";
+    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    timer = 0;
+    } 
+      else {
+        timer += deltaTime;
+    }
+    requestAnimationFrame(animate);
+}
+animate(0);
+
+window.addEventListener("resize", function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height)
+})
+
+
+
+//stats-chess.com
+const chess_elo = fetch("https://api.chess.com/pub/player/mrneko-og/stats")
+.then(response => response.json())
+.then(data => document.getElementById("chess").innerHTML = data.chess_rapid.last.rating)
+
+
+
+
+
+
